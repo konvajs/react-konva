93d41ab3ec8e2c272679af03a0fc03710d343cc8
Anton Lavrenov <lavrton@gmail.com>
2016-04-20 08:59:20 +0700

    production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var webpack = require('webpack');
 
 module.exports = {
     entry: [
@@ -26,10 +27,10 @@ module.exports = {
         ]
     },
     plugins: [
-        // new webpack.DefinePlugin({
-        //     'process.env': {
-        //         NODE_ENV: JSON.stringify('production')
-        //     }
-        // })
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
     ]
 };
