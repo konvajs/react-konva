7039d7c0fb2de6d3d018f969968ca9ebd6cb6f9a
Anton Lavrenov <lavrton@gmail.com>
2016-05-30 19:55:32 +0700

    typyings

---
+++ react-konva.d.ts
@@ -0,0 +1,28 @@
+// very stupid typings...
+
+export var Stage : any;
+export var Layer : any;
+export var FastLayer : any;
+
+export var Group : any;
+
+
+export var Rect : any;
+export var Label : any;
+
+export var Circle: any;
+export var Ellipse: any;
+export var Wedge: any;
+export var Line: any;
+export var Sprite: any;
+export var Image: any;
+export var Text: any;
+export var TextPath: any;
+export var Star: any;
+export var Ring: any;
+export var Arc: any;
+export var Tag: any;
+export var Path: any;
+export var RegularPolygon: any;
+export var Arrow: any;
+export var Shape: any;
