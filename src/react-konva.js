8158c023252388b6b81d3bf3abb55ed3c3195d15
Anton Lavrenov <lavrton@gmail.com>
2017-01-25 11:33:14 -0500

    remove var declaration

--- src/react-konva.js
+++ src/react-konva.js
@@ -221,8 +221,7 @@ var NodeMixin = {
       var val, prop;
       for(prop in updatedProps) {
         val = updatedProps[prop];
-	var Image = Image || window.Image;
-        if (val instanceof Image && !val.complete) {
+        if (val instanceof window.Image && !val.complete) {
           var node = this.node;
           val.addEventListener('load', function() {
             var layer = node.getLayer();
