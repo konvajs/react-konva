b813f133955a9c91b74aebba210783f809040265
Anton Lavrenov <lavrton@gmail.com>
2017-02-04 07:05:12 -0500

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,21 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: [
-        './src/bundle.js'
-    ],
-    output: {
-        path: __dirname,
-        filename: './dist/react-konva.bundle.js',
-        libraryTarget: "var",
-        library: "ReactKonva"
-    },
-    externals: {
-        'konva': 'Konva'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: ['babel'],
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    }
+  entry: [ './src/bundle.js' ],
+  output: {
+    path: __dirname,
+    filename: './dist/react-konva.bundle.js',
+    libraryTarget: 'var',
+    library: 'ReactKonva'
+  },
+  externals: { konva: 'Konva' },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: [ 'babel-loader?presets[]=es2015' ]
+      }
+    ]
+  }
 };
--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,29 +1,21 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: [
-        './test/tests.js'
-    ],
-    output: {
-        path: __dirname,
-        filename: './test/tests.bundle.js'
-    },
-    externals: {
-        'cheerio': 'window',
-        'react/addons': true,
-        'react/lib/ExecutionEnvironment': true,
-        'react/lib/ReactContext': true
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: ['babel'],
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            }
-        ]
-    }
+  entry: [ './test/tests.js' ],
+  output: { path: __dirname, filename: './test/tests.bundle.js' },
+  externals: {
+    cheerio: 'window',
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: [ 'babel-loader?presets[]=es2015,presets[]=react' ]
+      }
+    ]
+  }
 };
