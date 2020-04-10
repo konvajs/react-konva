38aefc41bfc859f3aee30a8ed96a7dbed5b71329
Anton Lavrenov <lavrton@gmail.com>
2020-04-10 10:13:06 -0500

    update deps

--- nwb.config.js
+++ nwb.config.js
@@ -2,10 +2,10 @@ module.exports = {
   type: 'react-component',
   npm: {
     esModules: false,
-    umd: false
+    umd: false,
   },
   // may be useful for debugging tests
   // karma: {
-  //   browsers: ['Chrome']
-  // }
+  //   browsers: ['Chrome'],
+  // },
 };
